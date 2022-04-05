c75f63d77e34c79e463c89ba6f314d2ef5815d38
Bao <thebaoking@gmail.com>
2022-04-04 23:32:05 -0700

    Forth commit, all logic compete and work on system print

--- src/inputHandler.java
+++ src/inputHandler.java
@@ -1,22 +1,27 @@
-import java.util.Locale;
-import java.util.Scanner;
+import java.util.*;
 
 public class inputHandler {
     String myBaodle;
     int myTotalTries = 5;
     boolean myCompleted = false;
 
+    public static final String ANSI_RESET = "\u001B[0m";
+    public static final String GREEN_BACKGROUND = "\u001B[42m";
+    public static final String YELLOW_BACKGROUND = "\u001B[43m";
+
+
+
     public inputHandler(String theBaodle) {
         this.myBaodle = theBaodle;
         System.out.println("Welcome to Baodle!");
+        System.out.println("Enter a 5 letters word: ");
         enterInput();
     }
 
     private void enterInput(){
         while(myTotalTries >= 0 && !myCompleted) {
-            System.out.println("Enter a 5 letters word: ");
             Scanner scan = new Scanner(System.in);
-            checkWord(scan.nextLine());
+            checkWord(scan.nextLine().toLowerCase());
             myTotalTries--;
         }
         if(myTotalTries == 0) {
@@ -25,19 +30,33 @@ public class inputHandler {
     }
 
     private void checkWord(String recievedWord){
-        if(recievedWord.toLowerCase(Locale.ROOT).equals(myBaodle)) {
+        if(recievedWord.toLowerCase().equals(myBaodle)) {
             System.out.println("Good job! You got the word correct");
             System.out.println("You got it in " + (6 - myTotalTries) + " tries");
             myCompleted = true;
+        } else {
+            hasLettersOrLocations(recievedWord);
         }
     }
 
-    private void hasWord(){
+    private void hasLettersOrLocations(String recievedWord){
+        char[] baodleArr = myBaodle.toCharArray();
+        char[] recieChars = recievedWord.toCharArray();
+        System.out.println("___________________________\n");
 
-    }
+        for (int i = 0; i < baodleArr.length; i++) {
+            for (int j = 0; j < recieChars.length; j++) {
 
-    private void hasPosition(){
-
+                if(i == j && baodleArr[i] == recieChars[j]) { //Correct location
+                    System.out.print(GREEN_BACKGROUND + baodleArr[i] + ANSI_RESET + " ");
+                } else if (baodleArr[i] == recieChars[j]) { // Correct letter
+                    System.out.print(YELLOW_BACKGROUND + baodleArr[i] +ANSI_RESET  + " ");
+                } else if (i == j) {
+                    System.out.print(" _ ");
+                }
+            }
+        }
+        System.out.println("\n");
     }
 
 }
